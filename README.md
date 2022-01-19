7564330a6deab0e52e1b94a4410e7803bbf9cd00
Ty Seddon <ty.seddon@cognivista.com>
2022-01-19 18:47:24 -0500

    Hello world example as a baseline implementation

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # joi-skill-music
+
+## Install Skill
+From the Raspberry Pi 
+    mycroft-msm https://github.com/TySeddon/joi-skill-music.git
+
+## Mycroft Terminology
+
+* **utterance** - An utterance is a phrase spoken by the User, after the User says the Wake Word. what's the weather like in Toronto? is an utterance.
+* **dialog** - A dialog is a phrase that is spoken by Mycroft. Different Skills will have different dialogs, depending on what the Skill does. For example, in a weather Skill, a dialog might be the.maximum.temperature.is.dialog.
+* **intent** - Mycroft matches utterances that a User speaks with a Skill by determining an intent from the utterance. For example, if a User speaks Hey Mycroft, what's the weather like in Toronto? then the intent will be identified as weather and matched with the Weather Skill. When you develop new Skills, you need to define new intents.
+
+## Virtual Environment Setup
+
+### Install Virtual Environment
+    pip install virtualenv
+
+### Creating 
+    python -m venv venv
+
+### Activate Virtual Environment
+    .\venv\Scripts\activate
+
+# Required Packages
+    pip install msk
+    pip install adapt-parser
+
+
+## Update requirements.txt
+
+    pip freeze > requirements.txt
+
+## Load Required Packages
+Create your virtual environment, then load all the required dependencies with:
+
+    pip install -r requirements.txt
---
+++ __init__.py
@@ -0,0 +1,73 @@
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+
+class JoiMusicSkill(MycroftSkill):
+    def __init__(self):
+        """ The __init__ method is called when the Skill is first constructed.
+        It is often used to declare variables or perform setup actions, however
+        it cannot utilise MycroftSkill methods as the class does not yet exist.
+        """
+        super().__init__()
+        self.learning = True
+
+    def initialize(self):
+        """ Perform any final setup needed for the skill here.
+        This function is invoked after the skill is fully constructed and
+        registered with the system. Intents will be registered and Skill
+        settings will be available."""
+        my_setting = self.settings.get('my_setting')
+
+    @intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
+    def handle_thank_you_intent(self, message):
+        """ This is an Adapt intent handler, it is triggered by a keyword."""
+        self.speak_dialog("welcome")
+
+    @intent_handler('HowAreYou.intent')
+    def handle_how_are_you_intent(self, message):
+        """ This is a Padatious intent handler.
+        It is triggered using a list of sample phrases."""
+        self.speak_dialog("how.are.you")
+
+    @intent_handler(IntentBuilder('HelloWorldIntent')
+                    .require('HelloWorldKeyword'))
+    def handle_hello_world_intent(self, message):
+        """ Skills can log useful information. These will appear in the CLI and
+        the skills.log file."""
+        self.log.info("There are five types of log messages: "
+                      "info, debug, warning, error, and exception.")
+        self.speak_dialog("hello.world")
+
+
+    def converse(self, utterances, lang):
+        """ The converse method can be used to handle follow up utterances 
+        prior to the normal intent handling process. It can be useful for handling 
+        utterances from a User that do not make sense as a standalone intent.
+        """
+        if utterances and self.voc_match(utterances[0], 'understood'):
+            self.speak_dialog('great')
+            return True
+        else:
+            return False        
+
+    def stop(self):
+        """ The stop method is called anytime a User says "Stop" or a similar command. 
+        It is useful for stopping any output or process that a User might want to end 
+        without needing to issue a Skill specific utterance such as media playback 
+        or an expired alarm notification.
+        """
+        pass
+
+    def shutdown(self):
+        """ The shutdown method is called during the Skill process termination. 
+        It is used to perform any final actions to ensure all processes and operations 
+        in execution are stopped safely. This might be particularly useful for Skills 
+        that have scheduled future events, may be writing to a file or database, 
+        or that have initiated new processes.
+        """
+        #self.cancel_scheduled_event('my_event')
+        #self.stop_my_subprocess()    
+        pass
+
+
+def create_skill():
+    return JoiMusicSkill()
