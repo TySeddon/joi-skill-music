758ab7cc50ec878b0414495f6df3177811e72232
Ty Seddon <ty.seddon@cognivista.com>
2022-02-16 19:16:13 -0500

    asyncio.run

--- __init__.py
+++ __init__.py
@@ -131,7 +131,7 @@ class JoiMusicSkill(MycroftSkill):
         return camera
 
     def _run_motion_detection(self, seconds_length):        
-        start_time, end_time, motion_event_pairs = self.motion_loop.run(self.camera_motion.read_camera_motion_async(seconds_length))
+        start_time, end_time, motion_event_pairs = asyncio.run(self.camera_motion.read_camera_motion_async(seconds_length), loop=self.motion_loop)
         self.log.info(motion_event_pairs)
 
     def start_motion_detection(self, seconds_length):
