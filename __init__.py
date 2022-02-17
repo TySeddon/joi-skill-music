ad4b1dd2db62e55ea89d986bc9fbbe022e3ff290
Ty Seddon <ty.seddon@cognivista.com>
2022-02-16 19:56:36 -0500

    thread join

--- __init__.py
+++ __init__.py
@@ -154,10 +154,10 @@ class JoiMusicSkill(MycroftSkill):
             # handle_motion_detect_done will be called once it has stopped
             self.log.info('stopping motion detection')
             self.camera_motion.cancel()
-            if hasattr(self, 'motion_thread') and self.motion_thread:
-                self.log.info('Joining thread')
-                self.motion_thread.join()
-                self.log.info('Thread joined')
+            # if hasattr(self, 'motion_thread') and self.motion_thread:
+            #     self.log.info('Joining thread')
+            #     self.motion_thread.join()
+            #     self.log.info('Thread joined')
 
     async def handle_motion_detect_done(self):
         self.log.info('handle_motion_detect_done')
