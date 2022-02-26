522237df7198459bea0c32d7560841b8f6fba6d3
Ty Seddon <ty.seddon@cognivista.com>
2022-02-26 16:32:30 -0500

    skill.joi-skill-mainmenu.show

--- __init__.py
+++ __init__.py
@@ -219,6 +219,8 @@ class JoiMusicSkill(MycroftSkill):
         wait_while_speaking()
         sleep(5)
         self.close_browser()
+        sleep(3)
+        self.bus.emit(Message("skill.joi-skill-mainmenu.show"))
 
     def song_intro(self, track, audio_features, motion_report):
         self.log.info("song_intro")
@@ -515,7 +517,9 @@ class JoiMusicSkill(MycroftSkill):
                 self.log.warn(f"Failed to pause of {self.player_name}")
         if self.play_state:
             self.play_state.is_playing = False
-        #self.close_browser()
+        self.close_browser()
+        sleep(3)
+        self.bus.emit(Message("skill.joi-skill-mainmenu.show"))
         self.stop_memorybox_session("stop")
         return True
 
